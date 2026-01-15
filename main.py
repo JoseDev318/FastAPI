77f1ee6f3e3bba105f4fe3f4ea36b6555c0bbe1c
JoseA318 <jose.arango@impocali.com>
2026-01-15 14:58:53 -0500

    Se agrega modelo.py y se utiliza metodo put

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import zoneinfo
-from datetime import datetime
-
+#import zoneinfo
+#from datetime import datetime
 from typing import Union
 from fastapi import FastAPI
-from models import Customer, Transaction, Invoice
+from modelo import item
+#from models import Customer, Transaction, Invoice
 
 
 #Creacion de una Aplicacion FastAPI:
@@ -25,34 +25,41 @@ def read_item(item_id:int, q: Union[str, None] = None):
 def calcular(operando_1:float, operando_2:float):
     return {"suma": operando_1 + operando_2}
 
-
-country_timezones = {
-    "CO": "America/Bogota",
-    "MX": "America/Mexico_City",
-    "AR": "America/Argentina/Buenos_Aires",
-    "BR": "America/Sao_Paulo",
-    "PE": "America/Lima",
-}
+@aplicacion.put("/items/{item_id}")
+def update_item(item_id: int, item: item ):
+    return {"item_name": item.name, "item_id": item_id, "item_price": item.price} 
 
 
-@aplicacion.get("/time/{iso_code}")
-async def time(iso_code: str):
-    iso = iso_code.upper()
-    timezone_str = country_timezones.get(iso)
-    tz = zoneinfo.ZoneInfo(timezone_str)
-    return {"time": datetime.now(tz)}
 
-@aplicacion.post("/costumers")
-async def create_customer(customer_data: Customer):
-    return customer_data
 
-@aplicacion.post("/transactions")
-async def create_transaction(transaction_data: Transaction):
-    return transaction_data
 
-@aplicacion.post("/invoices")
-async def create_invoice(invoice_data: Invoice):
-    return invoice_data
+# /*###country_timezones = {
+#     "CO": "America/Bogota",
+#     "MX": "America/Mexico_City",
+#     "AR": "America/Argentina/Buenos_Aires",
+#     "BR": "America/Sao_Paulo",
+#     "PE": "America/Lima",
+# }
+
+
+# @aplicacion.get("/time/{iso_code}")
+# async def time(iso_code: str):
+#     iso = iso_code.upper()
+#     timezone_str = country_timezones.get(iso)
+#     tz = zoneinfo.ZoneInfo(timezone_str)
+#     return {"time": datetime.now(tz)}
+
+# @aplicacion.post("/costumers")
+# async def create_customer(customer_data: Customer):
+#     return customer_data
+
+# @aplicacion.post("/transactions")
+# async def create_transaction(transaction_data: Transaction):
+#     return transaction_data
+
+# @aplicacion.post("/invoices")
+# async def create_invoice(invoice_data: Invoice):
+#     return invoice_data /*
 
     
     
